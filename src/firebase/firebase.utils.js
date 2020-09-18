f12de1dc5d458c29dbb6488e87be03433347edf5
AleksandarMijic <a.mijic90@hotmail.com>
2020-09-18 12:13:40 -0500

    updating app with reselect and memoized selectors

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyA-M5JS6wUN_z11iDelVNIyE5d_v0at-Pw",
+  authDomain: "e-shop-crownreactjs.firebaseapp.com",
+  databaseURL: "https://e-shop-crownreactjs.firebaseio.com",
+  projectId: "e-shop-crownreactjs",
+  storageBucket: "e-shop-crownreactjs.appspot.com",
+  messagingSenderId: "277318071113",
+  appId: "1:277318071113:web:e4234ca1e263e0e998a098",
+  measurementId: "G-TQBHZ57HSH"
 };
 
 firebase.initializeApp(config);
